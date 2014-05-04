be183e5b6efc6262ec7576ddf10f3bf68237bb04
Silthus <silthus@raid-craft.de>
2014-05-04 18:46:50 +0200

    actually send broadcast messages

--- src/main/java/de/raidcraft/achievements/listener/AchievementListener.java
+++ src/main/java/de/raidcraft/achievements/listener/AchievementListener.java
@@ -1,16 +1,10 @@
 package de.raidcraft.achievements.listener;
 
-import de.raidcraft.achievements.AchievementPlugin;
 import de.raidcraft.api.achievement.events.AchievementGainEvent;
 import de.raidcraft.api.action.trigger.Trigger;
-import de.raidcraft.api.language.Translator;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-
-import java.util.Arrays;
 
 /**
  * @author Silthus
@@ -25,26 +19,6 @@ public class AchievementListener extends Trigger implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onAchievementGain(AchievementGainEvent event) {
 
-        // broadcast if wanted
-        if (event.getAchievement().getTemplate().isBroadcasting()) {
-            Arrays.asList(Bukkit.getOnlinePlayers()).forEach(player -> {
-                        if (player.equals(event.getAchievement().getHolder().getType())) {
-                            Translator.msg(AchievementPlugin.class, player, "achievement.get",
-                                    ChatColor.YELLOW + "Du hast den Erfolg " + ChatColor.BLUE + ChatColor.UNDERLINE +
-                                            "{0}" + ChatColor.RESET + ChatColor.YELLOW + " erhalten.",
-                                    event.getAchievement().getDisplayName()
-                            );
-                        } else {
-                            Translator.msg(AchievementPlugin.class, player, "achievement.broadcast",
-                                    ChatColor.GREEN + "{0}" + ChatColor.YELLOW + " hat den Erfolg " + ChatColor.BLUE + ChatColor.UNDERLINE +
-                                            "{1}" + ChatColor.RESET + ChatColor.YELLOW + " erhalten.",
-                                    event.getAchievement().getHolder().getDisplayName(),
-                                    event.getAchievement().getDisplayName()
-                            );
-                        }
-                    }
-            );
-        }
         // inform other achievements
         informListeners("gain", event.getAchievement().getHolder().getType(), config ->
                 !config.isSet("achievement")
--- src/main/java/de/raidcraft/achievements/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/achievements/listener/PlayerListener.java
@@ -3,13 +3,18 @@ package de.raidcraft.achievements.listener;
 import de.raidcraft.achievements.AchievementPlugin;
 import de.raidcraft.api.achievement.Achievement;
 import de.raidcraft.api.achievement.AchievementHolder;
+import de.raidcraft.api.achievement.events.AchievementGainEvent;
+import de.raidcraft.api.language.Translator;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Arrays;
 
 /**
  * @author Silthus
@@ -21,6 +26,31 @@ public class PlayerListener implements Listener {
     public PlayerListener(AchievementPlugin plugin) {
 
         this.plugin = plugin;
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onAchievementGain(AchievementGainEvent event) {
+
+        // broadcast if wanted
+        if (event.getAchievement().getTemplate().isBroadcasting()) {
+            Arrays.asList(Bukkit.getOnlinePlayers()).forEach(player -> {
+                        if (player.equals(event.getAchievement().getHolder().getType())) {
+                            Translator.msg(AchievementPlugin.class, player, "achievement.get",
+                                    ChatColor.YELLOW + "Du hast den Erfolg " + ChatColor.BLUE + ChatColor.UNDERLINE +
+                                            "{0}" + ChatColor.RESET + ChatColor.YELLOW + " erhalten.",
+                                    event.getAchievement().getDisplayName()
+                            );
+                        } else {
+                            Translator.msg(AchievementPlugin.class, player, "achievement.broadcast",
+                                    ChatColor.GREEN + "{0}" + ChatColor.YELLOW + " hat den Erfolg " + ChatColor.BLUE + ChatColor.UNDERLINE +
+                                            "{1}" + ChatColor.RESET + ChatColor.YELLOW + " erhalten.",
+                                    event.getAchievement().getHolder().getDisplayName(),
+                                    event.getAchievement().getDisplayName()
+                            );
+                        }
+                    }
+            );
+        }
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
