e4d5999479bbfb4baef411fe55796333da89a8cc
Silthus <silthus@raid-craft.de>
2014-05-04 19:55:39 +0200

    moved player commands

--- src/main/java/de/raidcraft/achievements/commands/BaseCommands.java
+++ src/main/java/de/raidcraft/achievements/commands/BaseCommands.java
@@ -5,15 +5,7 @@ import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.NestedCommand;
 import de.raidcraft.achievements.AchievementPlugin;
-import de.raidcraft.api.achievement.Achievement;
-import de.raidcraft.api.achievement.AchievementHolder;
-import de.raidcraft.util.PaginatedResult;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.time.format.DateTimeFormatter;
-import java.util.stream.Collectors;
 
 /**
  * @author Silthus
@@ -31,25 +23,10 @@ public class BaseCommands {
             aliases = {"achievements", "rca"},
             desc = "Base player command for achievements"
     )
-    @NestedCommand(value = PlayerCommands.class, executeBody = true)
+    @NestedCommand(value = PlayerCommands.class, executeBody = false)
     public void achievements(CommandContext args, CommandSender sender) throws CommandException {
 
-        if (!(sender instanceof Player)) {
-            throw new CommandException("Only players can execute this command.");
-        }
-        // TODO: make fancy with custom inventory
-        AchievementHolder<Player> holder = plugin.getAchievementManager().getAchievementHolder((Player) sender);
-        new PaginatedResult<Achievement<Player>>("Datum: Achievement") {
-            @Override
-            public String format(Achievement<Player> entry) {
 
-                return ChatColor.YELLOW + "" + entry.getCompletionDate().toLocalDateTime()
-                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
-                        + ": " + ChatColor.AQUA + entry.getDisplayName();
-            }
-        }.display(sender, holder.getCompletedAchievements().stream()
-                .sorted((el, o) -> el.getCompletionDate().compareTo(o.getCompletionDate())).collect(Collectors.toList()),
-                args.getFlagInteger('p', 1));
     }
 
     @Command(
--- src/main/java/de/raidcraft/achievements/commands/PlayerCommands.java
+++ src/main/java/de/raidcraft/achievements/commands/PlayerCommands.java
@@ -6,12 +6,17 @@ import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.CommandPermissions;
 import de.raidcraft.achievements.AchievementPlugin;
 import de.raidcraft.achievements.database.TAchievementHolder;
+import de.raidcraft.api.achievement.Achievement;
+import de.raidcraft.api.achievement.AchievementHolder;
 import de.raidcraft.util.PaginatedResult;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Silthus
@@ -45,5 +50,31 @@ public class PlayerCommands {
                         + " (" + ChatColor.GREEN + entry.getPoints() + ChatColor.AQUA + ")";
             }
         }.display(sender, list, args.getFlagInteger('p', 1));
+    }
+
+    @Command(
+            aliases = {"list"},
+            desc = "Lists gained achievements",
+            flags = "p:"
+    )
+    @CommandPermissions("rcachievements.cmd.list")
+    public void list(CommandContext args, CommandSender sender) throws CommandException {
+
+        if (!(sender instanceof Player)) {
+            throw new CommandException("Only players can execute this command.");
+        }
+        // TODO: make fancy with custom inventory
+        AchievementHolder<Player> holder = plugin.getAchievementManager().getAchievementHolder((Player) sender);
+        new PaginatedResult<Achievement<Player>>("Datum: Achievement") {
+            @Override
+            public String format(Achievement<Player> entry) {
+
+                return ChatColor.YELLOW + "" + entry.getCompletionDate().toLocalDateTime()
+                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
+                        + ": " + ChatColor.AQUA + entry.getDisplayName();
+            }
+        }.display(sender, holder.getCompletedAchievements().stream()
+                        .sorted((el, o) -> el.getCompletionDate().compareTo(o.getCompletionDate())).collect(Collectors.toList()),
+                args.getFlagInteger('p', 1));
     }
 }
