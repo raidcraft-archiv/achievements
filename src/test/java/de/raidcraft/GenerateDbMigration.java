6e65736052e60bdba4fb454afa26a5788f96bde2
Michael Reichenbach <michael@mirenoba.com>
2018-09-30 18:48:41 +0200

    refactor(api): move raidcraft-api tables into own project

--- src/test/java/de/raidcraft/GenerateDbMigration.java
+++ src/test/java/de/raidcraft/GenerateDbMigration.java
@@ -1,10 +1,16 @@
 package de.raidcraft;
 
+import de.raidcraft.achievements.database.TAchievement;
+import de.raidcraft.achievements.database.TAchievementHolder;
+import de.raidcraft.achievements.database.TAchievementTemplate;
 import io.ebean.annotation.Platform;
+import io.ebean.config.ServerConfig;
 import io.ebean.dbmigration.DbMigration;
 import io.ebeaninternal.dbmigration.DefaultDbMigration;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerateDbMigration {
 
@@ -17,8 +23,16 @@ public class GenerateDbMigration {
      */
     public static void main(String[] args) throws IOException {
 
+        List<Class<?>> tables = new ArrayList<>();
+        tables.add(TAchievement.class);
+        tables.add(TAchievementHolder.class);
+        tables.add(TAchievementTemplate.class);
+
         DbMigration dbMigration = new DefaultDbMigration();
         dbMigration.setPlatform(Platform.MYSQL);
+        ServerConfig config = new ServerConfig();
+        config.setClasses(tables);
+        dbMigration.setServerConfig(config);
         // generate the migration ddl and xml
         // ... with EbeanServer in "offline" mode
         dbMigration.generateMigration();
